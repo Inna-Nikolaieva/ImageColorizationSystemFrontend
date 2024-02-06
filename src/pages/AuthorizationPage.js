0585d78266803c36d2bacf5375e1bbace730f0dc
Inna Nikolaieva <anastasiiainna@gmail.com>
2024-02-06 19:07:05 +0200

    A simple page design without functionality has been created.

---
+++ src/pages/AuthorizationPage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import AuthorizationBlock from "../components/AuthorizationBlock";
+
+const AuthorizationPage = () => {
+  return (
+    <div>
+      <Header />
+      <AuthorizationBlock />
+      <Footer />
+      </div>
+  )
+};
+
+export default AuthorizationPage;
